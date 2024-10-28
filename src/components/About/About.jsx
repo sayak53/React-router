393769a8876b9daa74912586730fcc9be7b8e1d1
sayak53 <sayakchakraborty928@gmail.com>
2024-10-28 20:13:38 +0530

    just adding some functionality

---
+++ src/components/About/About.jsx
@@ -0,0 +1,10 @@
+
+const About = () => {
+    return (
+        <div>
+            <h1>All about sex</h1>
+        </div>
+    );
+};
+
+export default About;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './App.css'
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Home from './components/Home/Home';
+import About from './components/About/About';
 
 const router = createBrowserRouter([
   {
@@ -13,11 +16,11 @@ const router = createBrowserRouter([
   },
   {
     path: "/Home",
-    element: <div>This is home</div>,
+    element: <Home></Home>
   },
   {
     path: "/About",
-    element: <div>This is all about</div>,
+    element: <About></About>
   },
 ]);
 createRoot(document.getElementById('root')).render(
