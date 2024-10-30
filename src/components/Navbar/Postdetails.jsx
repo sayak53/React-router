816ec8b3deab7dd768a32c312b63a1e2d0282008
sayak53 <sayakchakraborty928@gmail.com>
2024-10-30 13:34:52 +0530

    learned how to navigate

--- src/components/Navbar/Postdetails.jsx
+++ src/components/Navbar/Postdetails.jsx
@@ -1,15 +1,23 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 
 const Postdetails = () => {
-    const postdetails = useLoaderData();
-    const{body,id,title} = postdetails
-    return (
-        <div className="post-section">
-            <h2>Information of Post :{id}</h2>
-            <h2>Title: {title}</h2>
-            <h2>Body: {body}</h2>
-        </div>
-    );
+  const postdetails = useLoaderData();
+  const navigate = useNavigate();
+
+  const handleGoBack = () => {
+    navigate(-1);
+  };
+  const { body, id, title } = postdetails;
+  return (
+    <div className="post-section">
+      <h2>Information of Post :{id}</h2>
+      <h2>Title: {title}</h2>
+      <h2>Body: {body}</h2>
+      <div className="Outlet">
+        <button onClick={handleGoBack} className="btn">Go back to Posts Section</button>
+      </div>
+    </div>
+  );
 };
 
 export default Postdetails;
